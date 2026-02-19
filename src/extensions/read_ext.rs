ce5d0035f54c4115ea488d09a38ddfa734688a89
Matteo Monti <matteo.monti@yahoo.it>
2026-02-19 15:13:34 +0100

    Split `{WriteExt, ReadExt}`'s varint logic in signed and unsigned

--- src/extensions/read_ext.rs
+++ src/extensions/read_ext.rs
@@ -3,7 +3,8 @@ use std::io::{self, Read};
 #[allow(dead_code)]
 pub(crate) trait ReadExt {
     fn read_u8(&mut self) -> io::Result<u8>;
-    fn read_varint(&mut self) -> io::Result<u64>;
+    fn read_signed_varint(&mut self) -> io::Result<i64>;
+    fn read_unsigned_varint(&mut self) -> io::Result<u64>;
 }
 
 impl<R> ReadExt for R
@@ -18,7 +19,12 @@ where
         Ok(byte)
     }
 
-    fn read_varint(&mut self) -> io::Result<u64> {
+    fn read_signed_varint(&mut self) -> io::Result<i64> {
+        let unsigned = self.read_unsigned_varint()?;
+        Ok(((unsigned >> 1) as i64) ^ (-((unsigned & 1) as i64)))
+    }
+
+    fn read_unsigned_varint(&mut self) -> io::Result<u64> {
         let mut value = 0u64;
         let mut shift = 0;
 
@@ -44,18 +50,62 @@ where
 mod tests {
     use super::*;
     use crate::extensions::WriteExt;
-    use std::{cmp::max, iter};
+    use std::{
+        cmp::{max, min},
+        iter,
+    };
 
     #[test]
-    fn bijection() {
+    fn signed_bijection() {
+        // Positive branch
+
         let mut value = 0;
 
         loop {
             let mut buffer = Vec::new();
-            buffer.write_varint(value).unwrap();
+            buffer.write_signed_varint(value).unwrap();
             buffer.extend(iter::repeat_with(rand::random::<u8>).take(rand::random_range(0..32)));
 
-            assert_eq!(buffer.as_slice().read_varint().unwrap(), value);
+            assert_eq!(buffer.as_slice().read_signed_varint().unwrap(), value);
+
+            if value == i64::MAX {
+                break;
+            }
+
+            value = max(value.saturating_add(value / 16), value + 1);
+        }
+
+        // Negative branch
+
+        let mut value = 0;
+
+        loop {
+            let mut buffer = Vec::new();
+            buffer.write_signed_varint(value).unwrap();
+            buffer.extend(iter::repeat_with(rand::random::<u8>).take(rand::random_range(0..32)));
+
+            assert_eq!(buffer.as_slice().read_signed_varint().unwrap(), value);
+
+            if value == i64::MIN {
+                break;
+            }
+
+            value = min(value.saturating_add(value / 16), value - 1);
+        }
+    }
+
+    #[test]
+    fn unsigned_bijection() {
+        // Positive branch only
+
+        let mut value = 0;
+
+        loop {
+            let mut buffer = Vec::new();
+            buffer.write_unsigned_varint(value).unwrap();
+            buffer.extend(iter::repeat_with(rand::random::<u8>).take(rand::random_range(0..32)));
+
+            assert_eq!(buffer.as_slice().read_unsigned_varint().unwrap(), value);
 
             if value == u64::MAX {
                 break;
--- src/extensions/write_ext.rs
+++ src/extensions/write_ext.rs
@@ -3,7 +3,8 @@ use std::io::{self, Write};
 #[allow(dead_code)]
 pub(crate) trait WriteExt {
     fn write_u8(&mut self, value: u8) -> io::Result<()>;
-    fn write_varint(&mut self, value: u64) -> io::Result<()>;
+    fn write_signed_varint(&mut self, value: i64) -> io::Result<()>;
+    fn write_unsigned_varint(&mut self, value: u64) -> io::Result<()>;
 }
 
 impl<W> WriteExt for W
@@ -14,7 +15,12 @@ where
         self.write_all(&[value])
     }
 
-    fn write_varint(&mut self, mut value: u64) -> io::Result<()> {
+    fn write_signed_varint(&mut self, value: i64) -> io::Result<()> {
+        let unsigned = ((value >> 63) ^ (value << 1)) as u64;
+        self.write_unsigned_varint(unsigned)
+    }
+
+    fn write_unsigned_varint(&mut self, mut value: u64) -> io::Result<()> {
         for _ in 0..8 {
             if value >= 0x80 {
                 self.write_u8((value as u8) | 0x80)?;
