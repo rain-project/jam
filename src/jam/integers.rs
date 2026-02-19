d05ecf2f1b55cc8e897e15ba121efa2f04c41d48
Matteo Monti <matteo.monti@yahoo.it>
2026-02-19 23:18:37 +0100

    Implement `Jam` for all integers up to 64 bits

--- src/jam/integers.rs
+++ src/jam/integers.rs
@@ -36,3 +36,61 @@ impl Jam for u8 {
         writer.write_u8(*self)
     }
 }
+
+macro_rules! signed_impl {
+    ($type: ty) => {
+        impl Jam for $type {
+            fn read_from<R>(reader: &mut R) -> io::Result<Self>
+            where
+                R: Read,
+            {
+                reader
+                    .read_signed_varint()?
+                    .try_into()
+                    .map_err(|_| io::ErrorKind::InvalidData.into())
+            }
+
+            fn write_into<W>(&self, writer: &mut W) -> io::Result<()>
+            where
+                W: Write,
+            {
+                writer.write_signed_varint(*self as i64)
+            }
+        }
+    };
+}
+
+macro_rules! unsigned_impl {
+    ($type: ty) => {
+        impl Jam for $type {
+            fn read_from<R>(reader: &mut R) -> io::Result<Self>
+            where
+                R: Read,
+            {
+                reader
+                    .read_unsigned_varint()?
+                    .try_into()
+                    .map_err(|_| io::ErrorKind::InvalidData.into())
+            }
+
+            fn write_into<W>(&self, writer: &mut W) -> io::Result<()>
+            where
+                W: Write,
+            {
+                writer.write_unsigned_varint(*self as u64)
+            }
+        }
+    };
+}
+
+signed_impl!(i16);
+unsigned_impl!(u16);
+
+signed_impl!(i32);
+unsigned_impl!(u32);
+
+signed_impl!(i64);
+unsigned_impl!(u64);
+
+signed_impl!(isize);
+unsigned_impl!(usize);
