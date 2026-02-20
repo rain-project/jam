b392878b1285ff22e7549f9a4b6d1586f46b8b12
Matteo Monti <matteo.monti@yahoo.it>
2026-02-20 19:10:02 +0100

    Implement `Jam` for `Result`s

--- src/jam/enums.rs
+++ src/jam/enums.rs
@@ -42,3 +42,44 @@ where
         Ok(())
     }
 }
+
+impl<T, E> Jam for Result<T, E>
+where
+    T: Jam,
+    E: Jam,
+{
+    fn read_from<R>(reader: &mut R) -> io::Result<Self>
+    where
+        R: Read,
+    {
+        let variant = reader.read_u8()?;
+
+        let value = match variant {
+            0 => Ok(T::read_from(reader)?),
+            1 => Err(E::read_from(reader)?),
+
+            _ => return Err(io::ErrorKind::InvalidData.into()),
+        };
+
+        Ok(value)
+    }
+
+    fn write_into<W>(&self, writer: &mut W) -> io::Result<()>
+    where
+        W: Write,
+    {
+        match self {
+            Ok(value) => {
+                writer.write_u8(0)?;
+                value.write_into(writer)?;
+            }
+
+            Err(error) => {
+                writer.write_u8(1)?;
+                error.write_into(writer)?;
+            }
+        }
+
+        Ok(())
+    }
+}
