0de586a3aba33850b551a8f402646839b774f67b
Matteo Monti <matteo.monti@yahoo.it>
2026-02-22 00:46:19 +0100

    Scaffold `{Decode, PartialDecode}`

---
+++ src/decode/mod.rs
@@ -0,0 +1,37 @@
+use std::io::{self, Read};
+
+pub trait Decode: PartialDecode {
+    fn decode(mut bytes: &[u8]) -> io::Result<Self> {
+        <Self as Decode>::decode_from(&mut bytes)
+    }
+
+    fn decode_from<R>(reader: &mut R) -> io::Result<Self>
+    where
+        R: Read;
+}
+
+pub trait PartialDecode: Sized {
+    type Partial;
+
+    fn decode(mut bytes: &[u8]) -> io::Result<Self::Partial> {
+        Self::decode_from(&mut bytes)
+    }
+
+    fn decode_from<R>(reader: &mut R) -> io::Result<Self::Partial>
+    where
+        R: Read;
+
+    fn complete(partial: Self::Partial) -> Self;
+}
+
+impl<T> Decode for T
+where
+    T: PartialDecode<Partial = T>,
+{
+    fn decode_from<R>(reader: &mut R) -> io::Result<Self>
+    where
+        R: Read,
+    {
+        <Self as PartialDecode>::decode_from(reader)
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
 pub mod extensions;
 
+mod decode;
 mod encode;
 
-pub use self::encode::{Encode, StableEncode};
+pub use self::{
+    decode::{Decode, PartialDecode},
+    encode::{Encode, StableEncode},
+};
