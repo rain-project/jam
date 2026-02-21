3e906485b2b17fffef4d65590fad0cbcaea28de2
Matteo Monti <matteo.monti@yahoo.it>
2026-02-21 19:39:12 +0100

    Scaffold `PartialJam`

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ pub mod derive;
 pub mod extensions;
 
 mod jam;
+mod partial_jam;
 mod stable_jam;
 
-pub use self::{jam::Jam, stable_jam::StableJam};
+pub use self::{jam::Jam, partial_jam::PartialJam, stable_jam::StableJam};
